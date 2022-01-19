e6c34a4e476501f153f4f73b1ece22cb074e7141
Donald Labaj <dlabaj@redhat.com>
2022-01-19 11:55:06 -0500

    Added redux, services, and utilities folders.

---
+++ src/redux/actions/actions.ts
@@ -0,0 +1,96 @@
+import axios from 'axios';
+import { getBackendURL } from '../../utilities/utils';
+import { ThunkAction } from 'redux-thunk';
+import { Actions, AppNotification, AppState, GetUserAction } from '../types';
+import { Action } from 'redux';
+
+export const getUserPending = (): GetUserAction => ({
+  type: Actions.GET_USER_PENDING,
+  payload: {},
+});
+
+export const getUserFulfilled = (response: {
+  kube: { userName: string; clusterID: string; clusterBranding: string };
+}): GetUserAction => ({
+  type: Actions.GET_USER_FULFILLED,
+  payload: {
+    user: response.kube.userName,
+    clusterID: response.kube.clusterID,
+    clusterBranding: response.kube.clusterBranding,
+  },
+});
+
+export const getUserRejected = (error: Error): GetUserAction => ({
+  type: Actions.GET_USER_REJECTED,
+  payload: {
+    error,
+  },
+});
+
+export const detectUser = (): ThunkAction<
+  void,
+  AppState,
+  unknown,
+  Action<string>
+> => {
+  const url = getBackendURL('/api/status');
+  return async (dispatch) => {
+    dispatch(getUserPending());
+    try {
+      const response = await axios.get(url, {});
+      dispatch(getUserFulfilled(response.data));
+    } catch (e) {
+      dispatch(getUserRejected(e.response.data));
+    }
+  };
+};
+
+let notificationCount = 0;
+
+export const addNotification = (
+  notification: AppNotification,
+): ThunkAction<void, AppState, unknown, Action<string>> => {
+  return (dispatch) => {
+    dispatch({
+      type: Actions.ADD_NOTIFICATION,
+      payload: { notification: { ...notification, id: ++notificationCount } },
+    });
+  };
+};
+
+export const hideNotification = (
+  notification: AppNotification,
+): ThunkAction<void, AppState, unknown, Action<string>> => {
+  return (dispatch) => {
+    dispatch({ type: Actions.HIDE_NOTIFICATION, payload: { notification } });
+  };
+};
+
+export const ackNotification = (
+  notification: AppNotification,
+): ThunkAction<void, AppState, unknown, Action<string>> => {
+  return (dispatch) => {
+    dispatch({ type: Actions.ACK_NOTIFICATION, payload: { notification } });
+  };
+};
+
+export const removeNotification = (
+  notification: AppNotification,
+): ThunkAction<void, AppState, unknown, Action<string>> => {
+  return (dispatch) => {
+    dispatch({ type: Actions.REMOVE_NOTIFICATION, payload: { notification } });
+  };
+};
+
+export const forceComponentsUpdate = (): ThunkAction<
+  void,
+  AppState,
+  unknown,
+  Action<string>
+> => {
+  return (dispatch) => {
+    dispatch({
+      type: Actions.FORCE_COMPONENTS_UPDATE,
+    });
+  };
+};
---
+++ src/utilities/const.ts
@@ -0,0 +1,30 @@
+import { OdhDocumentType } from '../types';
+
+const DEV_MODE = process.env.APP_ENV === 'development';
+const API_PORT = process.env.BACKEND_PORT || 8080;
+const POLL_INTERVAL = process.env.POLL_INTERVAL
+  ? parseInt(process.env.POLL_INTERVAL)
+  : 30000;
+const DOC_LINK = process.env.DOC_LINK;
+const COMMUNITY_LINK = process.env.COMMUNITY_LINK;
+const SUPPORT_LINK = process.env.SUPPORT_LINK;
+
+export {
+  DEV_MODE,
+  API_PORT,
+  POLL_INTERVAL,
+  DOC_LINK,
+  COMMUNITY_LINK,
+  SUPPORT_LINK,
+};
+
+export const DOC_TYPE_TOOLTIPS = {
+  [OdhDocumentType.Documentation]:
+    'Technical information for using the service',
+  [OdhDocumentType.Tutorial]:
+    'End-to-end guides for solving business problems in data science',
+  [OdhDocumentType.QuickStart]: 'Step-by-step instructions and tasks',
+  [OdhDocumentType.HowTo]: 'Instructions and code for everyday procedures',
+};
+
+export const CATEGORY_ANNOTATION = 'opendatahub.io/categories';
