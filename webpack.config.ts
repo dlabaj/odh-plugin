5b3d5c3fb577b5d716c24dcd4fa7a9297d1f2596
Donald Labaj <dlabaj@redhat.com>
2022-02-08 17:03:38 -0500

    Updated webpack to pull in enviornment variables.

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,26 +1,29 @@
 /* eslint-env node */
 
-import { Configuration as WebpackConfiguration } from "webpack";
-import { Configuration as WebpackDevServerConfiguration } from "webpack-dev-server";
-import * as path from "path";
-import { ConsoleRemotePlugin } from "@openshift-console/dynamic-plugin-sdk-webpack";
+import { Configuration as WebpackConfiguration } from 'webpack';
+import { Configuration as WebpackDevServerConfiguration } from 'webpack-dev-server';
+import * as path from 'path';
+import { ConsoleRemotePlugin } from '@openshift-console/dynamic-plugin-sdk-webpack';
+import { setupWebpackDotenvFilesForEnv } from './config/dotenv';
+
+const RELATIVE_DIRNAME = path.resolve(__dirname, '.');
 
 interface Configuration extends WebpackConfiguration {
   devServer?: WebpackDevServerConfiguration;
 }
 
 const config: Configuration = {
-  mode: "development",
+  mode: 'development',
   // No regular entry points. The remote container entry is handled by ConsoleRemotePlugin.
   entry: {},
-  context: path.resolve(__dirname, "src"),
+  context: path.resolve(__dirname, 'src'),
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name]-bundle.js",
-    chunkFilename: "[name]-chunk.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name]-bundle.js',
+    chunkFilename: '[name]-chunk.js',
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
   },
   module: {
     rules: [
@@ -29,22 +32,22 @@ const config: Configuration = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "ts-loader",
+            loader: 'ts-loader',
             options: {
-              configFile: path.resolve(__dirname, "tsconfig.json"),
+              configFile: path.resolve(__dirname, 'tsconfig.json'),
             },
           },
         ],
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|woff2?|ttf|eot|otf)(\?.*$|$)/,
-        loader: "file-loader",
+        loader: 'file-loader',
         options: {
-          name: "assets/[name].[ext]",
+          name: 'assets/[name].[ext]',
         },
       },
       {
@@ -59,24 +62,31 @@ const config: Configuration = {
     static: './dist',
     port: 9001,
     headers: {
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-      "Access-Control-Allow-Headers": "X-Requested-With, Content-Type, Authorization"
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers':
+        'X-Requested-With, Content-Type, Authorization',
     },
   },
-  plugins: [new ConsoleRemotePlugin()],
-  devtool: "source-map",
+  plugins: [
+    ...setupWebpackDotenvFilesForEnv({
+      directory: RELATIVE_DIRNAME,
+      isRoot: true,
+    }),
+    new ConsoleRemotePlugin(),
+  ],
+  devtool: 'source-map',
   optimization: {
-    chunkIds: "named",
+    chunkIds: 'named',
     minimize: false,
   },
 };
 
-if (process.env.NODE_ENV === "production") {
-  config.mode = "production";
-  config.output.filename = "[name]-bundle-[hash].min.js";
-  config.output.chunkFilename = "[name]-chunk-[chunkhash].min.js";
-  config.optimization.chunkIds = "deterministic";
+if (process.env.NODE_ENV === 'production') {
+  config.mode = 'production';
+  config.output.filename = '[name]-bundle-[hash].min.js';
+  config.output.chunkFilename = '[name]-chunk-[chunkhash].min.js';
+  config.optimization.chunkIds = 'deterministic';
   config.optimization.minimize = true;
 }
 
